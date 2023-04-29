fe1b702602844cbe2f7deebfe62c04ca20b047b3
Daniel Nowrot <97291292+danielnowrot@users.noreply.github.com>
2023-04-29 14:40:26 +0200

    Update personCreditsSlice.js

--- src/features/personDetails/personCreditsSlice.js
+++ src/features/personDetails/personCreditsSlice.js
@@ -3,35 +3,43 @@ import { createSlice } from "@reduxjs/toolkit";
 const personCreditsSlice = createSlice({
   name: "personCredits",
   initialState: {
-    personCredits: null,
     status: "initial",
+    people: [],
+    totalPages: 0,
+    totalResults: 0,
   },
   reducers: {
-    axiosPersonCredits: () => ({
+    axiosPersonCreditsLoading: () => ({
       status: "loading",
-      personCredits: null,
     }),
-    axiosPersonCreditsSuccess: (_, { payload: personCredits }) => ({
+    axiosPersonCreditsSuccess: (_, { payload: people }) => ({
       status: "success",
-      personCredits,
+      people: people.results,
+      totalPages: people.total_pages,
+      totalResults: people.total_results,
     }),
     axiosPersonCreditsError: () => ({
       status: "error",
-      personCredits: null,
     }),
   },
 });
 
-const selectPersonCreditsState = (state) => state.personCredits;
+export const selectPersonCreditsState = (state) => state.personCredits;
 
 export const selectPersonCredits = (state) =>
-  selectPersonCreditsState(state).personCredits;
+  selectPersonCreditsState(state).people;
 
 export const selectPersonCreditsStatus = (state) =>
   selectPersonCreditsState(state).status;
+  
+export const selectPersonCreditsTotalPages = (state) =>
+  selectPersonCreditsState(state).totalPages;
+  
+export const selectPersonCreditsResults = (state) =>
+  selectPersonCreditsState(state).totalResults;
 
 export const {
-  axiosPersonCredits,
+  axiosPersonCreditsLoading,
   axiosPersonCreditsSuccess,
   axiosPersonCreditsError,
 } = personCreditsSlice.actions;
