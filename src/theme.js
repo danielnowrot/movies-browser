be17af5ade06b96efcbe724a6576ba995eff7ce0
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-04-29 11:31:02 +0200

    added colors & breakpoints

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,23 @@ const theme = {
         mediaThickS: 142,
         mediaSizeS: 470,
         mediaSizeL: 935
+    },
+    colors: {
+        whisper: "#F5F5FA",
+        white: "#FFFFFF",
+        black: "#000000",
+        woodsmoke: "#18181B",
+        stormGray: "#74788B",
+        mystic: "#E4E6F0",
+        silver: "#C4C4C4",
+        waterloo: "#7E839A",
+        pattensBlue: "#D6E4FF",
+        scienceBlue: "#0044CC",
+        snuff: "#DDDDEE",
+        linkWater: "#D3DEF3",
+    },
+    breakpoints: {
+        mobileMax: 767,
     }
 };
 
