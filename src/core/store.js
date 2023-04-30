efd9e2e770f38dff6f685a9a6236d91baa670b5b
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-05-01 01:11:36 +0200

    mixed store

--- src/core/store.js
+++ src/core/store.js
@@ -3,14 +3,11 @@ import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 import movieDetailsReducer from "../features/movieDetails/movieDetailsSlice";
 import movieListReducer from "../features/moveList/movieListSlice";
-<<<<<<< HEAD
 import genreListReducer from "../features/genreList/genreListSlice";
-=======
 import movieCreditsReducer from "../features/movieDetails/movieCreditsSlice";
 import personListReducer from "../features/personList/personListSlice";
 import personDetailsReducer from "../features/personDetails/personDetailsSlice";
 import personCreditsReducer from "../features/personDetails/personCreditsSlice";
->>>>>>> master
 
 const sagaMiddleWare = createSagaMiddleware();
 
@@ -18,14 +15,11 @@ const store = configureStore({
   reducer: {
     movieDetails: movieDetailsReducer,
     movieList: movieListReducer,
-<<<<<<< HEAD
     genreList: genreListReducer,
-=======
     movieCredits: movieCreditsReducer,
     personList: personListReducer,
     personDetails: personDetailsReducer,
     personCredits: personCreditsReducer,
->>>>>>> master
   },
   middleware: [sagaMiddleWare],
 });
