4b15873c8c94828ca4ab77a7adfe5c9a953ce2b9
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-05-14 12:43:32 +0200

    added movie tile for person-details page

---
+++ src/features/movieDetails/MovieTile/index.js
@@ -0,0 +1,48 @@
+import { Genre } from "./Genre";
+import {
+  DescriptionWrapper,
+  Subtitle,
+  ImageWrapper,
+  TileWrapper,
+  Rate,
+  RatingWrapper,
+  Star,
+  Title,
+  Votes,
+} from "./styled";
+import noPoster from "../../../Images/no poster.svg";
+
+export const MovieTile = ({
+  id,
+  title,
+  poster_path,
+  vote_average,
+  vote_count,
+  release_date,
+  genre_ids,
+}) => {
+  return (
+    <TileWrapper key={id} id={id}>
+      <ImageWrapper
+        src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : noPoster}
+        alt=""
+      ></ImageWrapper>
+      <DescriptionWrapper>
+        {title && <Title>{title}</Title>}
+        {release_date && (
+          <Subtitle>{new Date(release_date).getFullYear()}</Subtitle>
+        )}
+        {genre_ids && <Genre genre_ids={genre_ids} />}
+        {vote_average && vote_count ? (
+          <RatingWrapper>
+            <Star />
+            <Rate>{vote_average}</Rate>
+            <Votes>{vote_count} votes</Votes>
+          </RatingWrapper>
+        ) : (
+          <Star />
+        )}
+      </DescriptionWrapper>
+    </TileWrapper>
+  );
+};
