a94b61788d0c77c6ebb0dcbe93b75cc0cb7768cc
Daniel Nowrot <daniel.nowrot@gmail.com>
2023-05-14 12:50:41 +0200

    added person-details page

--- src/features/personDetails/PersonDetails/index.js
+++ src/features/personDetails/PersonDetails/index.js
@@ -1,20 +1,155 @@
 import { useDispatch, useSelector } from "react-redux";
-import { axiosPersonDetails, selectPersonDetails } from "../personDetailsSlice";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import {
+  axiosPersonDetails,
+  selectCast,
+  selectCrew,
+  selectPerson,
+  selectPersonDetailsStatus,
+} from "../personDetailsSlice";
 import { useEffect } from "react";
-import { selectPersonCredits, axiosPersonCredits } from "../personCreditsSlice";
+
+import { Tile } from "../../../core/Tile";
+import { ContentContainer, Wrapper } from "./styled";
+import { SectionTitle } from "../../../core/SectionTitle";
+import { MovieTile } from "../../movieDetails/MovieTile";
+import { fetchGenres } from "../../movieDetails/MovieTile/Genre/genreSlice";
+import { StyledLink } from "../../people/PeopleList/styled";
+import { Loading } from "../../../core/status/Loading";
+import { Error } from "../../../core/status/Error";
+import { searchQueryParamName } from "../../useQueryParameter";
+
 
 export const PersonDetails = () => {
   const dispatch = useDispatch();
-  const personDetails = useSelector(selectPersonDetails);
-  const personCredits = useSelector(selectPersonCredits);
+  const { id } = useParams();
+  const cast = useSelector(selectCast);
+  const crew = useSelector(selectCrew);
+  const person = useSelector(selectPerson);
+  const stateOfLoading = useSelector(selectPersonDetailsStatus);
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get(searchQueryParamName);
 
   useEffect(() => {
-    dispatch(axiosPersonDetails());
-  }, [dispatch]);
+    dispatch(axiosPersonDetails(id));
+    dispatch(fetchGenres());
+    if (query) {
+      navigate(`/people/${searchQueryParamName}=${query}`);
+    }
+  }, [dispatch, id, query, navigate]);
 
-  useEffect(() => {
-    dispatch(axiosPersonCredits());
-  }, [dispatch]);
+  return (
+    <>
+      {stateOfLoading === "loading" ? (
+        <Loading />
+      ) : stateOfLoading === "error" ? (
+        <Error />
+      ) : (
+        <Wrapper>
+          {person.map(
+            ({
+              profile_path,
+              name,
+              birthday,
+              place_of_birth,
+              biography,
+              id,
+            }) => (
+              <Tile
+                title={name}
+                profile_path={profile_path}
+                article={biography}
+                place_of_birth={place_of_birth}
+                birthday={birthday}
+                key={id}
+              />
+            )
+          )}
 
-  return <div>{/* Szczegóły o personie :) */}</div>;
+          {cast && cast.length > 0 && (
+            <>
+              <SectionTitle detailsPage>
+                Movies - cast {`(`}
+                {cast.length}
+                {`)`}
+              </SectionTitle>
+              <ContentContainer>
+                {cast.map(
+                  ({
+                    poster_path,
+                    id,
+                    credit_id,
+                    title,
+                    character,
+                    release_date,
+                    vote_average,
+                    vote_count,
+                    genre_ids,
+                  }) => (
+                    <li key={credit_id}>
+                      <StyledLink to={`/movies/${id}`}>
+                        <MovieTile
+                          key={credit_id}
+                          id={id}
+                          title={title}
+                          vote_average={vote_average}
+                          vote_count={vote_count}
+                          poster_path={poster_path}
+                          genre_ids={genre_ids}
+                          character={character}
+                          year={release_date}
+                        />
+                      </StyledLink>
+                    </li>
+                  )
+                )}
+              </ContentContainer>
+            </>
+          )}
+
+          {crew && crew.length > 0 && (
+            <>
+              <SectionTitle>
+                Movies - crew {`(`}
+                {crew.length}
+                {`)`}
+              </SectionTitle>
+              <ContentContainer>
+                {crew.map(
+                  ({
+                    job,
+                    title,
+                    vote_average,
+                    vote_count,
+                    release_date,
+                    poster_path,
+                    id,
+                    credit_id,
+                    genre_ids,
+                  }) => (
+                    <li key={credit_id}>
+                      <StyledLink to={`/movies/${id}`}>
+                        <MovieTile
+                          key={credit_id}
+                          id={id}
+                          title={title}
+                          vote_average={vote_average}
+                          vote_count={vote_count}
+                          job={job}
+                          year={release_date}
+                          poster_path={poster_path}
+                          genre_ids={genre_ids}
+                        />
+                      </StyledLink>
+                    </li>
+                  )
+                )}
+              </ContentContainer>
+            </>
+          )}
+        </Wrapper>
+      )}
+    </>
+  );
 };
